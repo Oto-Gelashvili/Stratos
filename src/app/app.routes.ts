465d43505ce9c67bf9f8dda344790254dbd41659
otogelashvili <otarbasilevsi@gmail.com>
2026-03-24 20:50:40 +0400

    init: starting styles + templates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'lobby', pathMatch: 'full' },
+  {
+    path: 'lobby',
+    loadComponent: () => import('./features/lobby/lobby').then((m) => m.Lobby),
+  },
+  { path: 'guide', loadComponent: () => import('./features/guide/guide').then((m) => m.Guide) },
+  {
+    path: 'heroes',
+    loadComponent: () => import('./features/heroes/heroes').then((m) => m.Heroes),
+  },
+  { path: '**', redirectTo: 'lobby' },
+];
---
+++ src/app/features/heroes/heroes.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  imports: [],
+  templateUrl: './heroes.html',
+  styleUrl: './heroes.css',
+})
+export class Heroes {}
