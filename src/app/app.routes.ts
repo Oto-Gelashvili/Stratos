dcef75c2e2d97b1642cfc5db7b04bd40cc22fed3
otogelashvili <otarbasilevsi@gmail.com>
2026-03-29 18:34:43 +0400

    feat: add layouts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,36 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'lobby', pathMatch: 'full' },
   {
-    path: 'lobby',
-    loadComponent: () => import('./features/lobby/lobby').then((m) => m.Lobby),
+    path: '',
+    loadComponent: () =>
+      import('./shared/layouts/public-layout/public-layout').then((m) => m.PublicLayout),
+    children: [
+      { path: '', redirectTo: 'lobby', pathMatch: 'full' },
+      {
+        path: 'lobby',
+        loadComponent: () => import('./features/lobby/lobby').then((m) => m.Lobby),
+      },
+      {
+        path: 'guide',
+        loadComponent: () => import('./features/guide/guide').then((m) => m.Guide),
+      },
+      {
+        path: 'heroes',
+        loadComponent: () => import('./features/heroes/heroes').then((m) => m.Heroes),
+      },
+    ],
   },
-  { path: 'guide', loadComponent: () => import('./features/guide/guide').then((m) => m.Guide) },
   {
-    path: 'heroes',
-    loadComponent: () => import('./features/heroes/heroes').then((m) => m.Heroes),
-  },
-  {
-    path: 'auth',
-    loadComponent: () => import('./features/sign-up/sign-up').then((m) => m.SignUp),
+    path: '',
+    loadComponent: () =>
+      import('./shared/layouts/auth-layout/auth-layout').then((m) => m.AuthLayout),
+    children: [
+      {
+        path: 'auth',
+        loadComponent: () => import('./features/sign-up/sign-up').then((m) => m.SignUp),
+      },
+    ],
   },
   { path: '**', redirectTo: 'lobby' },
 ];
---
+++ src/app/shared/layouts/public-layout/public-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Header } from '../../components/header/header';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-public-layout',
+  imports: [Header, RouterOutlet],
+  templateUrl: './public-layout.html',
+  styleUrl: './public-layout.css',
+})
+export class PublicLayout {}
